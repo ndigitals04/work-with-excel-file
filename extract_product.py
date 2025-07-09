338a4ca147a60baeafd613340da77683acdac301
ndigitals <ndigitals001@gmail.com>
2025-07-09 02:05:35 +0100

    Finished product

---
+++ extract_product.py
@@ -0,0 +1,16 @@
+import openpyxl
+workbook = openpyxl.load_workbook("C:\\Users\\USER\\Downloads\\product-list.xlsx")
+product_sheet = workbook["product-list"]
+product_list = []
+for row in range(1, product_sheet.max_row + 1):
+    product_list.append(product_sheet.cell(row,2).value)
+print("Appending to list completed")
+product_file = open("C:/users/user/desktop/product_list.txt", "w")
+for product in product_list:
+    if product == None:
+        pass
+    else:
+        product_file.write(product + "\n")
+
+product_file.close()
+print("Done")
